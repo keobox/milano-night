61eb05cf54ce8a09151a864b5b7f1192b7b497cf
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2024-04-30 14:45:01 +0200

    Tidy up demo 2

--- demos/02_kopf_crd/03_t3_apply_crd.sh
+++ demos/02_kopf_crd/03_t3_apply_crd.sh
@@ -4,16 +4,22 @@ export KUBECONFIG=~/.kube/config.kwok
 export PYTHONUNBUFFERED=1
 
 echo "Going to create the Custom Resource Definition"
-echo "Hit a key"
+echo "kubectl apply -f crd.yaml"
+echo "Hit enter"
 read
 kubectl apply -f crd.yaml
 
+echo
 echo "Going to create the Custom Resource"
-echo "Hit a key"
+echo "kubectl apply -f cr.yaml"
+echo "Hit enter"
 read
 kubectl apply -f cr.yaml
 
+echo
 echo "Get the resource"
+echo "kubectl get kopfexample -o yaml"
+echo "Hit enter"
 read
-kubectl get kopfexample
+kubectl get kopfexample -o yaml
 
